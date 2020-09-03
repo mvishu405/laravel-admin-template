f5a08154a6ddd04fee5a5ad6e05d4f0fd6d67c3c
mvishal <mvishu405@gmail.com>
2020-09-03 20:20:33 +0530

    working on forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
-
 require('./bootstrap');
+require('../../public/backend/plugins/bootstrap/js/bootstrap.bundle.min.js');
+require('../../public/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js');
+require('../../public/backend/dist/js/adminlte.js');
+require('../../public/backend/plugins/jquery-mousewheel/jquery.mousewheel.js');
+require('../../public/backend/plugins/raphael/raphael.min.js');
+require('jquery-mapael');
+require('chart.js');
 
 /**
  * Next, we will create a fresh React component instance and attach it to
